21282e3c27374beed204f2ec84c0fa34eabdd78f
saeeedahmedanas0336-prog <saeedahmedanas0336@gmail.com>
2026-07-29 23:01:56 +0500

    Update app.py

--- app.py
+++ app.py
@@ -39,3 +39,49 @@ if uploaded_file is not None:
 
     st.subheader("Extracted Text")
     st.text(extracted_text)
+if uploaded_file is not None and answer_key.strip() != "":
+
+    key_lines = [line.strip() for line in answer_key.split("\n") if line.strip()]
+    student_lines = [line.strip() for line in extracted_text.split("\n") if line.strip()]
+
+    total_questions = len(key_lines)
+    correct_answers = 0
+
+    st.subheader("Result")
+
+    for i in range(total_questions):
+
+        key_answer = re.sub(
+            r'^Q\d+[:.]?\s*',
+            '',
+            key_lines[i],
+            flags=re.IGNORECASE
+        ).strip()
+
+        if i < len(student_lines):
+            student_answer = student_lines[i]
+        else:
+            student_answer = "No Answer"
+
+        similarity = difflib.SequenceMatcher(
+            None,
+            key_answer.lower(),
+            student_answer.lower()
+        ).ratio()
+
+        if similarity >= 0.70:
+            st.success(f"Question {i+1}: Correct")
+            correct_answers += 1
+        else:
+            st.error(f"Question {i+1}: Wrong")
+
+        st.write("Correct Answer:", key_answer)
+        st.write("Student Answer:", student_answer)
+        st.write(f"Match: {similarity*100:.0f}%")
+        st.write("---")
+
+    percentage = (correct_answers / total_questions) * 100 if total_questions > 0 else 0
+
+    st.header("Final Score")
+    st.success(f"Marks: {correct_answers}/{total_questions}")
+    st.info(f"Percentage: {percentage:.2f}%")
